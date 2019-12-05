7f68c6f36625bf4c15ecb46c79caaa5623f4e48b
ElApseR <medussa2818@gmail.com>
2019-12-05 19:48:29 +0900

    Add batch loader module

--- modules.py
+++ modules.py
@@ -3,10 +3,12 @@ import numpy as np
 import torch 
 from torch.utils.data import Dataset, DataLoader
 import imageio
+import pickle
 
 class DanceImageDataset(Dataset):
     """
     generate dance image dataset
+    TODO: interpolation
     """
     def __init__(self, image_dir, stick_dir, list_pkl_file, transform=None):
         """
@@ -19,18 +21,19 @@ class DanceImageDataset(Dataset):
         """
         self.image_dir = image_dir
         self.stick_dir = stick_dir
-        self.list_pkl_file = list_pkl_file
         self.transform = transform
+        with open(list_pkl_file, 'rb') as f:
+            self.filename_list=pickle.load(f)  
 
     def __len__(self):
-        return len(self.list_pkl_file)
+        return len(self.filename_list)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_name = os.path.join(self.image_dir, self.list_pkl_file[idx])
-        stick_name = os.path.join(self.stick_dir, self.list_pkl_file[idx])
+        img_name = os.path.join(self.image_dir, self.filename_list[idx])
+        stick_name = os.path.join(self.stick_dir, self.filename_list[idx])
         image = imageio.imread(img_name)
         stick = imageio.imread(stick_name)
 
@@ -40,3 +43,54 @@ class DanceImageDataset(Dataset):
             sample = self.transform(sample)
 
         return sample
+
+
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, stick = sample['image'], sample['stick']
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img_transform = transform.resize(image, (new_h, new_w))
+        stk_transform = transform.resize(stick, (new_h, new_w))
+
+        # h and w are swapped for landmarks because for images,
+        # x and y axes are axis 1 and 0 respectively
+
+        return {'image': img_transform, 'stick': stk_transform}
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, stick = sample['image'], sample['stick']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+        stick = stick.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image),
+                'stick': torch.from_numpy(stick)}
